a2d5bed1573dd81d0a588ecb68817bcec7d57dc0
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2020-05-11 20:35:02 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,31 +1,24 @@
 # Создать менеджер контекста который будет подсчитывать время выполнения блока инструкций
-import sys
 from datetime import datetime
 import time
 
 
 class Manager:
-    def __init__(self, first, last):
-        self.first = first
-        self.last = last
+    def __init__(self):
         self.start = datetime.now()
         self.finish = None
 
     def __enter__(self):
-        self.res = []
-        for i in range(self.first, self.last):
-            i *= i+1
-            self.res.append(i)
-            time.sleep(0.5)
-        return self.res
-
-    def __exit__(self):
+        time.sleep(2)
         self.finish = datetime.now() - self.start
-        return '\ntime spent: {}\n'.format(self.finish)
+        return 'time spent: {}'.format(self.finish)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
-m1 = Manager(0, 9)
-m2 = Manager(9, -1)
+with Manager() as m1:
+    print('m1', m1)
 
-print('m1:', m1.__enter__(), m1.__exit__(),
-      '\nm2:', m2.__enter__(),  m2.__exit__())
+with Manager() as m2:
+    print('m2', m2)
