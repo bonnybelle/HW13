fd957addc4c985e2a828dc49f70a7daece8a2d7e
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2020-05-11 20:26:21 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,4 @@
 # Написать декоратор и менеджер контекста который будет подавлять все ошибки возникающие в теле вашей функции.
-import sys
 
 
 def error_decorator(function_to_decorate):
@@ -17,7 +16,7 @@ def error_decorator(function_to_decorate):
 
 @error_decorator
 def my_function():
-    i = int(input('value: '))
+    i = int('asd')
     return i
 
 
@@ -27,27 +26,25 @@ print(my_function)
 class Manager:
     print('----MANAGER----')
 
-    def __init__(self, value1, value2, action):
-        self.value1 = value1
-        self.value2 = value2
-        self.action = action
+    def __init__(self, *args):
+        self.args = args
 
     def __enter__(self):
         try:
-            self.res = self.action(self.value1, self.value2)
+            self.res = self.args[0] / self.args[1]
+            return self.res
         except Exception as e:
             self.res = e
+            print(e)
         return self.res
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         if self.res is Exception:
             print(self.res)
-            sys.exit()
 
 
-m1 = Manager(5, 'asd', lambda a, b: a / b)
-m2 = Manager(5, 0, lambda a, b: a / b)
-m3 = Manager(5, 2, lambda a, b: a / b)
-print('m1:', m1.__enter__(),
-      '\nm2:', m2.__enter__(),
-      '\nm3:', m3.__enter__())
+with Manager(5, 'asd') as m1:
+    print()
+
+with Manager(5, 0) as m2:
+    print()
